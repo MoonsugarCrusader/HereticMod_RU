4e7c5e2345a9fb2d02c2bc4475b141a21765506f
Moffein <22742925+Moffein@users.noreply.github.com>
2022-10-30 00:40:27 -0700

    Fixed unlock condition unlocking on all survivors.

--- Heretic/HereticPlugin.cs
+++ Heretic/HereticPlugin.cs
@@ -12,7 +12,7 @@ namespace HereticMod
 {
     [BepInDependency("com.rune580.riskofoptions")]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Moffein.Heretic", "Heretic", "1.1.1")]
+    [BepInPlugin("com.Moffein.Heretic", "Heretic", "1.1.2")]
     [R2API.Utils.R2APISubmoduleDependency(nameof(RecalculateStatsAPI), nameof(ContentAddition), nameof(ItemAPI), nameof(PrefabAPI), nameof(LoadoutAPI))]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
     public class HereticPlugin : BaseUnityPlugin
